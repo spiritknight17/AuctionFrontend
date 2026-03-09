551c0ad53f0c3fddf083d2cb642e42ebf98f537f
Barusu <payormatthewjoshguerrero@gmail.com>
2026-03-09 22:34:37 +0800

    Push

---
+++ app/src/main/java/com/payamanan/auctionfrontend/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.payamanan.auctionfrontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.payamanan.auctionfrontend.ui.theme.AuctionFrontendTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AuctionFrontendTheme {
+                Scaffold(modifier = Modifi er.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AuctionFrontendTheme {
+        Greeting("Android")
+    }
+}
